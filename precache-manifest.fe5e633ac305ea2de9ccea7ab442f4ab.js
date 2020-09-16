aa467fbd003939deec2a1f59353e50d26d6d051d
Liam McAteer <liam@liammcateer.nz>
2020-09-16 22:36:53 +1200

    Updates

---
+++ precache-manifest.fe5e633ac305ea2de9ccea7ab442f4ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6fd46ffcbdd0638f6e093fb74a1d65",
+    "url": "/routine-app/index.html"
+  },
+  {
+    "revision": "ffa51c1619aaa8f0c047",
+    "url": "/routine-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c98112a046ed2f4c8c1d",
+    "url": "/routine-app/static/js/2.40376209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/routine-app/static/js/2.40376209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffa51c1619aaa8f0c047",
+    "url": "/routine-app/static/js/main.52fc1b47.chunk.js"
+  },
+  {
+    "revision": "7212578601ab55ecd0ad",
+    "url": "/routine-app/static/js/runtime-main.ffff416b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/routine-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
